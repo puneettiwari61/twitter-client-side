67d01069f0a4f99d7a6e7beb65020aba13ac980b
Puneet <puneettiwari61@gmail.com>
2020-02-20 05:15:23 +0530

    home page twitter ui

--- src/App.js
+++ src/App.js
@@ -1,26 +1,30 @@
 import React from 'react';
-import logo from './logo.svg';
+import Aside from './components/Aside'
+import Nav from './components/nav/Nav'
+import Main from './components/main/Main'
+import Notifications from './components/nav/Notifications'
+import Explore from './components/nav/Explore'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+    <Router>
+    <header class="header flex">
+    <Nav />
+    <Route path='/' exact><Main /></Route>
+    <Route path='/explore' exact>< Explore/></Route>
+    <Route path='/notifications' exact><Notifications /></Route>
+
+    <Aside />
+    </header>
+    </Router>
+    </>
+    );
 }
 
 export default App;
---
+++ src/components/Aside.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+// import {browserRouter as Router} from "react-router-dom"
+
+
+function Aside() {
+  return (
+<div class="aside-div top">
+<aside>
+  Aside
+</aside>
+</div>
+  )
+}
+
+
+export default Aside
